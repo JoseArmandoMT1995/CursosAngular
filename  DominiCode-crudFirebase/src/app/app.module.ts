b925609a54d1e62537f6cb9de9e7c71b3fcef9a8
Armando <armando.moreno.tolentino@gmail.com>
2021-03-16 17:37:31 -0600

    DominiCode minuto 32:46 pasar datos en ruta

---
+++  DominiCode-crudFirebase/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { HeaderModule } from './shared/components/header/header.module';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { FooterModule } from './shared/components/footer/footer.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HeaderModule,
+    FooterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++  DominiCode-crudFirebase/src/app/pages/employees/list/list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router,NavigationExtras } from '@angular/router';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+  navigationExtras:NavigationExtras={
+    state:{
+      value:null
+    }
+  };
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+  mandarEditar(item: any):void{
+    this.navigationExtras.state.value= item;
+    this.router.navigate(['edit'],this.navigationExtras);
+  }
+  mandarVer(item: any):void{
+    this.navigationExtras.state.value= item;
+    this.router.navigate(['details'],this.navigationExtras);
+  }
+  mandarEliminar(item: any):void{
+    
+    alert("mensaje Eliminado");
+  }
+}
